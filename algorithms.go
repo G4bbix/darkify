2ac2d2867a44e68fdea45ec6f6666551b5723ea8
gabriel.winkler <gabriel.winkler@bpm.ch>
2023-10-31 07:56:20 +0100

    split distribute_leftovers to multiple func

--- algorithms.go
+++ algorithms.go
@@ -4,6 +4,79 @@ import (
 	"math"
 )
 
+func get_amount_zeros(color [3]uint8) uint8 {
+
+	var amount_zeros uint8
+	for _, segment := range color {
+		if segment == 0 {
+			amount_zeros++
+		}
+	}
+	return amount_zeros
+}
+
+func calc_remaining_unused_space(color [3]uint8) uint16 {
+	var remaining_unused_space uint16
+	for _, segment := range color {
+		if segment != 255 {
+			remaining_unused_space += uint16(255 - segment)
+		}
+	}
+	return remaining_unused_space
+}
+
+func calc_leftover_dist_two_zeros(color [3]uint8) [3]float64 {
+	var dist [3]float64
+	for i, segment := range color {
+		if segment != 255 {
+			dist[i] = 0.5
+		} else {
+			dist[i] = 0
+		}
+	}
+	return dist
+}
+
+// With one zero e.G. [255, 0, 128] we use 255 - segment as the dist template sum -> (255-255) + (255-0) + (255-128)
+func calc_dist_template_sum(color [3]uint8) uint16 {
+	var dist_template uint16
+	for _, segment := range color {
+		dist_template += (255 - uint16(segment))
+	}
+	return dist_template
+}
+
+func calc_leftover_dist_one_zero(color [3]uint8, remaining_unused_space uint16) [3]float64 {
+	var dist [3]float64
+	segment_to_flip := 4
+	for i, segment := range color {
+		if segment != 255 {
+			if segment_to_flip == 4 {
+				dist[i] = float64(255-segment) / float64(remaining_unused_space)
+				segment_to_flip = i
+			} else {
+				dist[i] = dist[segment_to_flip]
+				dist[segment_to_flip] = float64(255-segment) / float64(remaining_unused_space)
+			}
+		} else {
+			dist[i] = 0
+		}
+	}
+	return dist
+}
+
+func calc_leftover_dist_no_zeros(color [3]uint8, remaining_unused_space uint16) [3]float64 {
+	var dist [3]float64
+	for i, segment := range color {
+		if segment != 255 {
+			dist[i] = float64(segment) / float64(remaining_unused_space)
+		} else {
+			dist[i] = 0
+		}
+	}
+	return dist
+}
+
 // Get the percieved birghtness of the sum of the point between the light bg and dark bg to ITU BT.601
 func Calc_midpoint_sum(light_bg_rgb [3]uint8, dark_bg_rgb [3]uint8) float32 {
 	return (((float32(light_bg_rgb[0]) + float32(dark_bg_rgb[0])) * 0.299) +
@@ -22,7 +95,20 @@ func Darkify(light_rgb [3]uint8, mid_point_sum float32) [3]uint8 {
 	distribution := calc_distribution(light_rgb, sum_light_rgb)
 	dark_rgb, leftover := initCalc(sum_dark_rgb, light_rgb, distribution)
 	if leftover > 0 {
-		distribute_leftovers(&dark_rgb, leftover)
+		amount_zeros := get_amount_zeros(dark_rgb)
+
+		remaining_unused_space := calc_remaining_unused_space(dark_rgb)
+
+		var dist [3]float64
+		if amount_zeros == 1 {
+			dist_template := calc_dist_template_sum(dark_rgb)
+			dist = calc_leftover_dist_one_zero(dark_rgb, dist_template)
+		} else if amount_zeros == 2 {
+			dist = calc_leftover_dist_two_zeros(dark_rgb)
+		} else {
+			dist = calc_leftover_dist_no_zeros(dark_rgb, remaining_unused_space)
+		}
+		distribute_leftovers(&dark_rgb, leftover, dist)
 	}
 	return dark_rgb
 }
@@ -68,54 +154,27 @@ func initCalc(dark_rgb_sum uint16, light_rgb [3]uint8, dist [3]float64) ([3]uint
 	return dark_rgb, leftover
 }
 
-func distribute_leftovers(color *[3]uint8, leftover uint16) {
-	// If 2 nulls set dist to 0, 0.5, 0.5
-	var remainingDistSum int16 = 0
-	amount_zeros := 0
-
-	// Calculate sum of all segments to recieve redistribution
-	for _, segment := range *color {
-		if segment != 255 {
-			if segment == 0 {
-				amount_zeros += 1
-			}
-			remainingDistSum += int16(255 - segment)
-		}
-	}
+func distribute_leftovers(color *[3]uint8, leftover uint16, dist [3]float64) {
 
 	// Calculate new distribution
-	var dist [3]float64
-	for i, segment := range *color {
-		if segment != 255 {
-			if amount_zeros == 2 {
-				dist[i] = 0.5
-			} else if amount_zeros == 1 {
-				dist[i] = 1
-			} else {
-				dist[i] = float64(remainingDistSum) / float64(segment)
-			}
-		} else {
-			dist[i] = 0
-		}
-	}
-
 	var val uint16
-	var leftover_to_dist uint16
 	var leftover_distributed uint16
 	var color_with_leftover [3]uint8
 
 	for i := range *color {
-		if dist[i] != 255 {
-			leftover_to_dist = uint16(float64(leftover) * dist[i])
-			leftover_distributed += leftover_to_dist
+		if dist[i] != 0 {
+			leftover_to_dist := uint16(float64(leftover) * dist[i])
 			val = leftover_to_dist + uint16(color[i])
 			if val > 255 {
+				leftover_distributed += val - uint16(color[i])
 				color[i] = 255
 			} else {
 				color[i] = uint8(val)
+				leftover_distributed += leftover_to_dist
 			}
 		}
 	}
+
 	var remaining_leftover uint16 = leftover - leftover_distributed
 	if remaining_leftover > 0 {
 		for i, segment := range *color {
